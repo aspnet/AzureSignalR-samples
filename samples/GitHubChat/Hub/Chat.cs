26bf33ee8b246c8228611cfe4a19f749c8d0a182
Kevin Zhao <kevinzha@microsoft.com>
2018-04-26 09:48:31 +0800

    minor clean up

--- samples/GitHubChat/Hub/Chat.cs
+++ samples/GitHubChat/Hub/Chat.cs
@@ -1,11 +1,10 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using Microsoft.AspNetCore.Authorization;
-
 namespace Microsoft.Azure.SignalR.Samples.ChatRoom
 {
     using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.SignalR;
 
     [Authorize]
