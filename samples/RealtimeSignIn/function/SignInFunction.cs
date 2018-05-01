0ba714e5132743315db5a2cb20c2c954fc68bee1
Ken Chen <kenchen@microsoft.com>
2018-05-02 00:44:09 +0800

    implement rest api call to overcome dependency issue

--- samples/RealtimeSignIn/function/SignInFunction.cs
+++ samples/RealtimeSignIn/function/SignInFunction.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -49,47 +48,46 @@ namespace RealtimeSignIn
         [FunctionName("signin")]
         public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")]HttpRequestMessage req, TraceWriter log)
         {
-            var ua = Parser.GetDefault().Parse(req.Headers.UserAgent.ToString());
-            var os = ua.OS.Family;
-            var browser = ua.UserAgent.Family;
-
-            /*
-            var os = req.GetQueryNameValuePairs().FirstOrDefault(q => q.Key == "os").Value;
-            var browser = req.GetQueryNameValuePairs().FirstOrDefault(q => q.Key == "browser").Value;
-            dynamic data = await req.Content.ReadAsAsync<object>();
-            os = os ?? data?.os;
-            browser = browser ?? data?.browser;
-
-            if (os == null) return req.CreateErrorResponse(HttpStatusCode.BadRequest, "Missing os");
-            if (browser == null) return req.CreateErrorResponse(HttpStatusCode.BadRequest, "Missing browser");*/
-
-            var account = CloudStorageAccount.Parse(Environment.GetEnvironmentVariable("TableConnectionString"));
-            var client = account.CreateCloudTableClient();
-            var table = client.GetTableReference("SignInInfo");
-            var newInfo = new SignInInfo(os, browser);
-            var insert = TableOperation.Insert(newInfo);
-            table.Execute(insert);
-
-            var query = new TableQuery<SignInInfo>();
-            var stats = new SignInStats();
-            foreach (var info in table.ExecuteQuery(query))
+            try
             {
-                stats.totalNumber++;
-                if (!stats.byBrowser.ContainsKey(info.Browser)) stats.byBrowser[info.Browser] = 0;
-                if (!stats.byOS.ContainsKey(info.OS)) stats.byOS[info.OS] = 0;
-                stats.byBrowser[info.Browser]++;
-                stats.byOS[info.OS]++;
+                SignInHub.logger = log;
+                var ua = Parser.GetDefault().Parse(req.Headers.UserAgent.ToString());
+                var os = ua.OS.Family;
+                var browser = ua.UserAgent.Family;
+                var account = CloudStorageAccount.Parse(Environment.GetEnvironmentVariable("TableConnectionString"));
+                var client = account.CreateCloudTableClient();
+                var table = client.GetTableReference("SignInInfo");
+                var newInfo = new SignInInfo(os, browser);
+                var insert = TableOperation.Insert(newInfo);
+                table.Execute(insert);
+
+                var query = new TableQuery<SignInInfo>();
+                var stats = new SignInStats();
+                foreach (var info in table.ExecuteQuery(query))
+                {
+                    stats.totalNumber++;
+                    if (!stats.byBrowser.ContainsKey(info.Browser)) stats.byBrowser[info.Browser] = 0;
+                    if (!stats.byOS.ContainsKey(info.OS)) stats.byOS[info.OS] = 0;
+                    stats.byBrowser[info.Browser]++;
+                    stats.byOS[info.OS]++;
+                }
+
+                var result = new SignInResult()
+                {
+                    AuthInfo = SignInHub.GetAuthInfo(),
+                    Stats = stats
+                };
+
+                var status = await SignInHub.UpdateSignInStatsAsync(stats);
+                log.Info($"Status: {status}");
+
+                return req.CreateResponse(status, result, "application/json");
             }
-
-            var result = new SignInResult()
+            catch (Exception ex)
             {
-                AuthInfo = SignInHub.GetAuthInfo(),
-                Stats = stats
-            };
-
-            await SignInHub.UpdateSignInStats(stats);
-
-            return req.CreateResponse(HttpStatusCode.OK, result, "application/json");
+                log.Info(ex.ToString());
+                throw;
+            }
         }
     }
 }
