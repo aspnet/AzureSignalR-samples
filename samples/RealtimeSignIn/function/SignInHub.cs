704b8173d472bb1d5570c672e4e49ef5ee6c3952
Ken Chen <kenchen@microsoft.com>
2018-05-02 00:44:09 +0800

    implement rest api call to overcome dependency issue

--- samples/RealtimeSignIn/function/SignInHub.cs
+++ samples/RealtimeSignIn/function/SignInHub.cs
@@ -1,7 +1,14 @@
 ﻿using System;
-using System.Linq;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
-using Microsoft.Azure.SignalR;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
 
 namespace RealtimeSignIn
 {
@@ -13,28 +20,99 @@ namespace RealtimeSignIn
 
     static class SignInHub
     {
-        private const string HubName = "signIn";
+        private static readonly JwtSecurityTokenHandler jwtTokenHandler = new JwtSecurityTokenHandler();
+
+        private static readonly HttpClient httpClient = new HttpClient();
+
+        private const string ServiceUrl = "https://signalr-chat.servicedev.signalr.net";
+
+        private const string AccessKey = "1Y3YMtHTqYFLQzBd8ni/8jXRzp3giDs+oqJ+IrZSV98=";
+
+        private const string HubName = "signin";
 
         private static readonly string connectionString = Environment.GetEnvironmentVariable("AzureSignalRConnectionString");
 
-        private static readonly EndpointProvider endpointProvider = CloudSignalR.CreateEndpointProviderFromConnectionString(connectionString);
+        public static TraceWriter logger;
 
-        private static readonly TokenProvider tokenProvider = CloudSignalR.CreateTokenProviderFromConnectionString(connectionString);
-
-        private static readonly HubProxy hubProxy = CloudSignalR.CreateHubProxyFromConnectionString(connectionString, HubName);
-
-        public static async Task UpdateSignInStats(SignInStats stats)
+        class PayloadMessage
         {
-            await hubProxy.Clients.All.SendAsync("updateSignInStats", new object[] { stats });
+            public string Target { get; set; }
+
+            public object[] Arguments { get; set; }
+
+            public string[] ExcludedList { get; set; }
+        }
+
+        private static Task<HttpResponseMessage> PostJsonAsync(string url, object body, string bearer)
+        {
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri(url)
+            };
+
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", bearer);
+            request.Headers.Accept.Clear();
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            request.Headers.AcceptCharset.Clear();
+            request.Headers.AcceptCharset.Add(new StringWithQualityHeaderValue("UTF-8"));
+
+            var content = JsonConvert.SerializeObject(body);
+            request.Content = new StringContent(content, Encoding.UTF8, "application/json");
+            logger.Info(url);
+            logger.Info(content);
+            return httpClient.SendAsync(request);
+        }
+
+        private static async Task<HttpStatusCode> SendAsync(string hub, string method, params object[] args)
+        {
+            var payload = new PayloadMessage()
+            {
+                Target = method,
+                Arguments = args
+            };
+            var url = $"{ServiceUrl}:5002/api/v1-preview/hub/{hub}";
+            var bearer = GenerateJwtBearer(null, url, null, DateTime.UtcNow.AddMinutes(30), AccessKey);
+            var response = await PostJsonAsync(url, payload, bearer);
+            return response.StatusCode;
+        }
+
+        public static async Task<HttpStatusCode> UpdateSignInStatsAsync(SignInStats stats)
+        {
+            return await SendAsync(HubName, "updateSignInStats", stats.totalNumber, stats.byOS, stats.byBrowser);
         }
 
         public static AuthInfo GetAuthInfo()
         {
+            var hubUrl = $"{ServiceUrl}:5001/client/?hub={HubName}";
             return new AuthInfo()
             {
-                ServiceUrl = endpointProvider.GetClientEndpoint(HubName),
-                AccessToken = tokenProvider.GenerateClientAccessToken(HubName)
+                ServiceUrl = hubUrl,
+                AccessToken = GenerateJwtBearer(null, hubUrl, null, DateTime.UtcNow.AddMinutes(30), AccessKey)
             };
+        }
+
+        private static string GenerateJwtBearer(
+            string issuer = null,
+            string audience = null,
+            ClaimsIdentity subject = null,
+            DateTime? expires = null,
+            string signingKey = null)
+        {
+            SigningCredentials credentials = null;
+            if (!string.IsNullOrEmpty(signingKey))
+            {
+                var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(signingKey));
+                credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            }
+
+            var token = jwtTokenHandler.CreateJwtSecurityToken(
+                issuer: issuer,
+                audience: audience,
+                subject: subject,
+                expires: expires,
+                signingCredentials: credentials);
+            return jwtTokenHandler.WriteToken(token);
         }
     }
 }
