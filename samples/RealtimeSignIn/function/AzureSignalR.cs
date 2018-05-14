20a90469c475d2ec76b3a8193923c83c3fe3a52e
Kevin Zhao <kvn.zhl@gmail.com>
2018-05-14 15:15:57 +0800

    Update dependencies to latest version (#12)

--- samples/RealtimeSignIn/function/AzureSignalR.cs
+++ samples/RealtimeSignIn/function/AzureSignalR.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.IdentityModel.Tokens;
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.IdentityModel.Tokens;
 using Newtonsoft.Json;
 using System;
 using System.IdentityModel.Tokens.Jwt;
