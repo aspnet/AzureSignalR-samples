e3ecb36b5ccd8b6d7cbba2b4a7cc5a8ba11fa4c7
Chenyang Liu <chenyl@microsoft.com>
2018-06-13 18:22:09 +0800

    Update full sample

---
+++ samples/FullSample/Auth/FullAuthorizedHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+
+namespace Microsoft.Azure.SignalR.Samples.ChatRoom
+{
+    public class FullAuthorizedHandler : AuthorizationHandler<FullAuthorizedRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context,
+            FullAuthorizedRequirement requirement)
+        {
+            if (context.User.IsInRole("Admin")
+                && context.User.HasClaim(c => 
+                    c.Type == "AuthorizedUser" && c.Value.StartsWith("cookie")))
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
