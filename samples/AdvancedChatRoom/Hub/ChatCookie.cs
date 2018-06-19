e724f23de23694e318e235588211b866ae313203
Chenyang Liu <zackliu1995@hotmail.com>
2018-06-19 14:58:43 +0800

    Add Advanced Chat Room (#33)

---
+++ samples/AdvancedChatRoom/Hub/ChatCookie.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Microsoft.Azure.SignalR.Samples.AdvancedChatRoom
+{
+    [Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme)]
+    [Authorize(Roles = "Admin")]
+    [Authorize(Policy = "ClaimBasedAuth")]
+    [Authorize(Policy = "PolicyBasedAuth")]
+    public class ChatCookie : ChatBase
+    {
+    }
+}
---
+++ samples/AdvancedChatRoom/Startup.cs
@@ -0,0 +1,81 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Microsoft.Azure.SignalR.Samples.AdvancedChatRoom
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthorization(option =>
+            {
+                option.AddPolicy("ClaimBasedAuth", policy =>
+                    {
+                        policy.RequireClaim(ClaimTypes.NameIdentifier);
+                    });
+                option.AddPolicy("PolicyBasedAuth", policy => policy.Requirements.Add(new PolicyBasedAuthRequirement()));
+            });
+
+            services.AddSingleton<IAuthorizationHandler, PolicyBasedAuthHandler>();
+
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(option =>
+                {
+                    option.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidIssuer = JwtController.Issuer,
+                        ValidAudience = JwtController.Audience,
+                        IssuerSigningKey = JwtController.SigningCreds.Key
+                    };
+                });
+
+            services.AddMvc();
+            services.AddSignalR()
+                .AddAzureSignalR(options =>
+            {
+                options.ClaimsProvider = context => new[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier, context.Request.Query["username"])
+                };
+            });
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseAuthentication();
+            app.UseMvc();
+            app.UseFileServer();
+            app.UseAzureSignalR(routes =>
+            {
+                routes.MapHub<ChatJwt>("/chatjwt");
+                routes.MapHub<ChatCookie>("/chatcookie");
+            });
+        }
+    }
+}
