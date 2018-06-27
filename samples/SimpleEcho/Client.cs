b90f7219bb36dd8029a7ac19235ee42981aa9405
Xiangyang Yu <xianyu@microsoft.com>
2018-06-27 18:45:22 +0800

    Add a sample of simple echo.

---
+++ samples/SimpleEcho/Client.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace Microsoft.Azure.SignalR.Samples.SimpleEcho
+{
+    public class Client
+    {
+        private const string message = "Hello!";
+        private readonly TaskCompletionSource<string> resp = new TaskCompletionSource<string>();
+
+        public async Task Run()
+        {
+            try
+            {
+                var connection = new HubConnectionBuilder().WithUrl("http://localhost:5000/echo").Build();
+                connection.On<string>("echo", _resp => resp.SetResult(_resp));
+                await connection.StartAsync();
+                await connection.InvokeAsync<string>("echo", message);
+                Console.WriteLine(await resp.Task);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex);
+                throw;
+            }
+        }
+    }
+}
---
+++ samples/SimpleEcho/EchoHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Microsoft.Azure.SignalR.Samples.SimpleEcho
+{
+    public class EchoHub : Hub
+    {
+        public void Echo(string message)
+        {
+            Clients.Client(Context.ConnectionId).SendAsync("echo", message);
+        }
+    }
+}
