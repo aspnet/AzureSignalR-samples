7c0bb63a2f315494d64292c19d686cb0d055c215
Xiangyang Yu <xianyu@microsoft.com>
2018-06-29 17:25:55 +0800

    Some changes.

--- samples/SimpleEcho/EchoHub.cs
+++ samples/SimpleEcho/EchoHub.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.AspNetCore.SignalR;
 
 namespace Microsoft.Azure.SignalR.Samples.SimpleEcho
 {
