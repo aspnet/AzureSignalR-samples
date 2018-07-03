9e7c1bcbce3804923d45ffb5089caf0a527baf5e
Chenyang Liu <chenyl@microsoft.com>
2018-07-03 18:02:09 +0800

    Add Rest Sample

---
+++ samples/ConsoleSample/ConsoleSample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.Extensions.CommandLineUtils;
+
+namespace ConsoleSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var app = new CommandLineApplication();
+            app.FullName = "Azure SignalR Rest Sample";
+            app.HelpOption("-h|--help");
+
+            app.OnExecute(() =>
+            {
+                
+                return 0;
+            });
+
+            var server = new ServerHandler();
+            server.SendToUser("cookie-a");
+
+
+        }
+    }
+}
