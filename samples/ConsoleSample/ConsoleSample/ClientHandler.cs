4d218e367fb16d20d85aa19c7ec13a5479fdfc08
Chenyang Liu <chenyl@microsoft.com>
2018-07-05 17:55:07 +0800

    Update

---
+++ samples/ConsoleSample/ConsoleSample/ClientHandler.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Azure.SignalR.Sample.ConsoleSample;
+using Microsoft.Extensions.CommandLineUtils;
+
+namespace ConsoleSample
+{
+    public class ClientHandler
+    {
+        private readonly HubConnection _connection;
+
+        public ClientHandler(string connectionString, string hubName, string userId)
+        {
+            var serviceUtils = new ServiceUtils(connectionString);
+
+            var url = GetClientUrl(serviceUtils.Endpoint, hubName);
+
+            _connection = new HubConnectionBuilder()
+                .WithUrl(url, option =>
+                {
+                    option.AccessTokenProvider = () =>
+                    {
+                        return Task.FromResult(serviceUtils.GenerateClientAccessToken(url, new Claim[]
+                        {
+                            new Claim(ClaimTypes.NameIdentifier, userId),
+                        }));
+                    };
+                }).Build();
+
+            _connection.On<string, string>("SendMessage",
+                (string server, string message) =>
+                {
+                    Console.WriteLine($"Received message from server {server}: {message}");
+                });
+        }
+
+        public async Task StartAsync()
+        {
+            await _connection.StartAsync();
+        }
+
+        private string GetClientUrl(string endpoint, string hubName)
+        {
+            return $"{endpoint}:5001/client/?hub={hubName}";
+        }
+    }
+}
--- samples/ConsoleSample/ConsoleSample/ServerHandler.cs
+++ samples/ConsoleSample/ConsoleSample/ServerHandler.cs
@@ -1,121 +1,182 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.IdentityModel.Tokens.Jwt;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http.Connections.Client;
+using Microsoft.Azure.SignalR.Sample.ConsoleSample;
+using Microsoft.Extensions.CommandLineUtils;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.ObjectPool;
 using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
 
 namespace ConsoleSample
 {
     public class ServerHandler
     {
-        private const string Endpoint = "https://testrest.service.signalr.net";
-        private const string HubName = "ChatCookie";
-        private const string URL = "";
-        private const string AccessKey = "G4EHMq0ALOocnGp9G7OIECVe4Qkumcv69buSDgOodQg=";
-        private static readonly JwtSecurityTokenHandler JwtTokenHandler = new JwtSecurityTokenHandler();
-        private static readonly HttpClient client = new HttpClient();
+        private static readonly HttpClient Client = new HttpClient();
+        private static string _serverName;
+        private readonly ServiceUtils ServiceUtils;
+        private string _hubName;
+        private string _endpoint;
 
-        public ServerHandler()
+        private readonly PayloadMessage DefaultPayloadMessage;
+
+        public ServerHandler(string connectionString, string hubName)
         {
-            
-        }
+            _serverName = GenerateServerName();
 
-        public async void SendToUser(string userId)
-        {
-            string connectionString = Guid.NewGuid().ToString();
-            //var httpConnectionOptions = new HttpConnectionOptions
-            //{
-            //    Url = GetServiceUrl($"{GetRestUrl(5002, HubName)}/user/{userId}", connectionString),
-            //    AccessTokenProvider = () => Task.FromResult(GenerateServerAccessToken(HubName, userId)),
-            //};
+            ServiceUtils = new ServiceUtils(connectionString);
+            _hubName = hubName;
+            _endpoint = ServiceUtils.Endpoint;
 
-            var request = new HttpRequestMessage(HttpMethod.Post, GetServiceUrl($"{GetRestUrl(5002, HubName)}/user/{userId}", connectionString));
-            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", GenerateServerAccessToken(HubName, userId));
-            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-            await client.SendAsync(request);
-
-        }
-
-        public void SendToUsers()
-        {
-
-        }
-
-        public void BroadCast()
-        {
-
-        }
-
-        public void SendToGroup()
-        {
-
-        }
-
-        public void SendToGroups()
-        {
-
-        }
-
-        private string GenerateServerAccessToken(string hubName, string userId, TimeSpan? lifetime = null)
-        {
-            IEnumerable<Claim> claims = null;
-            if (userId != null)
+            DefaultPayloadMessage = new PayloadMessage
             {
-                claims = new[]
+                Target = "SendMessage",
+                Arguments = new[]
                 {
-                    new Claim(ClaimTypes.NameIdentifier, userId)
-                };
+                    _serverName,
+                    "Sent from rest api call",
+                }
+            };
+        }
+
+        public async Task Start()
+        {
+            ShowHelp();
+            while (true)
+            {
+                var argLine = Console.ReadLine();
+                if (argLine == null)
+                {
+                    continue;
+                }
+                var args = argLine.Split(' ');
+
+                if (args.Length == 1 && args[0].Equals("broadcast"))
+                {
+                    await BroadCast(_hubName);
+                }
+                else if (args.Length == 3 && args[0].Equals("send"))
+                {
+                    switch (args[1])
+                    {
+                        case "user":
+                            await SendToUser(_hubName, args[2]);
+                            break;
+                        case "users":
+                            await SendToUsers(_hubName, args[2]);
+                            break;
+                    }
+                }
             }
-
-            return GenerateAccessTokenCore(GetEndpoint(5002, "server", HubName), claims, TimeSpan.FromHours(1));
         }
 
-        private string GenerateAccessTokenCore(string audience, IEnumerable<Claim> claims, TimeSpan lifetime)
+        public async Task SendToUser(string hubName, string userId)
         {
-            var expire = DateTime.UtcNow.Add(lifetime);
+            try
+            {
+                var url = GetSendToUserUrl(hubName, userId);
+                var request = BuildRequest(url);
 
-            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(AccessKey));
-            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
-
-            var token = JwtTokenHandler.CreateJwtSecurityToken(
-                issuer: null,
-                audience: audience,
-                subject: new ClaimsIdentity(claims),
-                expires: expire,
-                signingCredentials: credentials);
-            return JwtTokenHandler.WriteToken(token);
+                var response = await Client.SendAsync(request);
+                if (response.StatusCode != HttpStatusCode.Accepted)
+                {
+                    throw new Exception("sent error");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
 
-        private string GetEndpoint(int port, string path, string hubName)
+        public async Task SendToUsers(string hubName, string userList)
         {
-            return $"{Endpoint}:{port}/{path}/?hub={hubName.ToLower()}";
+            var url = GetSendToUsersUrl(hubName, userList);
+            var request = BuildRequest(url);
+            var response = await Client.SendAsync(request);
+            if (response.StatusCode != HttpStatusCode.Accepted)
+            {
+                Console.WriteLine($"Sent error: {response.StatusCode}");
+            }
         }
 
-        private Uri GetServiceUrl(string baseUrl, string connectionId)
+        public async Task BroadCast(string hubName)
         {
-            var baseUri = new UriBuilder(baseUrl);
-            //var query = "cid=" + connectionId;
-            //if (baseUri.Query != null && baseUri.Query.Length > 1)
-            //{
-            //    baseUri.Query = baseUri.Query.Substring(1) + "&" + query;
-            //}
-            //else
-            //{
-            //    baseUri.Query = query;
-            //}
-            return baseUri.Uri;
+            var url = GetBroadcastUrl(hubName);
+            var request = BuildRequest(url);
+            var response = await Client.SendAsync(request);
+            if (response.StatusCode != HttpStatusCode.Accepted)
+            {
+                Console.WriteLine($"Sent error: {response.StatusCode}");
+            }
         }
 
-        private string GetRestUrl(int port, string hubName)
+        private Uri GetUrl(string baseUrl)
         {
-            return $"{Endpoint}:{port}/api/v1-preview/hub/{hubName.ToLower()}";
+            return new UriBuilder(baseUrl).Uri;
         }
+
+        private string GetSendToUserUrl(string hubName, string userId)
+        {
+            return $"{GetBaseUrl(hubName)}/user/{userId}";
+        }
+
+        private string GetSendToUsersUrl(string hubName, string userList)
+        {
+            return $"{GetBaseUrl(hubName)}/users/{userList}";
+        }
+
+        private string GetBroadcastUrl(string hubName)
+        {
+            return $"{GetBaseUrl(hubName)}";
+        }
+
+        private string GetBaseUrl(string hubName)
+        {
+            return $"{_endpoint}:5002/api/v1-preview/hub/{hubName.ToLower()}";
+        }
+
+        private string GenerateServerName()
+        {
+            // Use the machine name for convenient diagnostics, but add a guid to make it unique.
+            // Example: MyServerName_02db60e5fab243b890a847fa5c4dcb29
+            return $"{Environment.MachineName}_{Guid.NewGuid():N}";
+        }
+
+        private HttpRequestMessage BuildRequest(string url)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Post, GetUrl(url));
+
+            request.Headers.Authorization =
+                new AuthenticationHeaderValue("Bearer", ServiceUtils.GenerateServerAccessToken(url, _serverName));
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            request.Content = new StringContent(JsonConvert.SerializeObject(DefaultPayloadMessage), Encoding.UTF8, "application/json");
+
+            return request;
+        }
+
+        private void ShowHelp()
+        {
+            Console.WriteLine("*********Usage*********\n" +
+                              "send user <User Id>\n" +
+                              "send users <User Id List>\n" + 
+                              "broadcase\n" +
+                              "***********************");
+        }
+    }
+
+    public class PayloadMessage
+    {
+        public string Target { get; set; }
+
+        public object[] Arguments { get; set; }
     }
 }
