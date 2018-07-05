34718b7d3ebc83e9d16efdbe98c936e4564cc867
Chenyang Liu <chenyl@microsoft.com>
2018-07-05 17:55:07 +0800

    Update

--- samples/ConsoleSample/ConsoleSample/Program.cs
+++ samples/ConsoleSample/ConsoleSample/Program.cs
@@ -9,18 +9,61 @@ namespace ConsoleSample
         {
             var app = new CommandLineApplication();
             app.FullName = "Azure SignalR Rest Sample";
-            app.HelpOption("-h|--help");
+            app.HelpOption("--help");
 
-            app.OnExecute(() =>
+            app.Command("client", cmd =>
             {
-                
-                return 0;
+                cmd.Description = "Start a client to listen to the service";
+                cmd.HelpOption("--help");
+
+                var connectionString = cmd.Option("-c|--connectionstring", "Set ConnectionString", CommandOptionType.SingleValue);
+                var hub = cmd.Option("-h|--hub", "Set hub", CommandOptionType.SingleValue);
+                var userId = cmd.Argument("<userId>", "Set User ID");
+
+                cmd.OnExecute(async () =>
+                {
+                    if (!connectionString.HasValue() || !hub.HasValue())
+                    {
+                        MissOptions();
+                        return 0;
+                    }
+
+                    var client = new ClientHandler(connectionString.Value(), hub.Value(), userId.Value);
+                    await client.StartAsync();
+                    Console.WriteLine("Client started...");
+                    Console.ReadLine();
+                    return 0;
+                });
             });
 
-            var server = new ServerHandler();
-            server.SendToUser("cookie-a");
+            app.Command("server", cmd =>
+            {
+                cmd.Description = "Start a server to send message through RestAPI";
+                cmd.HelpOption("--help");
 
+                var connectionString = cmd.Option("-c|--connectionstring", "Set ConnectionString", CommandOptionType.SingleValue);
+                var hub = cmd.Option("-h|--hub", "Set hub", CommandOptionType.SingleValue);
 
+                cmd.OnExecute(async () =>
+                {
+                    if (!connectionString.HasValue() || !hub.HasValue())
+                    {
+                        MissOptions();
+                        return 0;
+                    }
+
+                    var server = new ServerHandler(connectionString.Value(), hub.Value());
+                    await server.Start();
+                    return 0;
+                });
+            });
+
+            app.Execute(args);
+        }
+
+        private static void MissOptions()
+        {
+            Console.WriteLine("Miss required options: ConnectionString and Hub must be set");
         }
     }
 }
