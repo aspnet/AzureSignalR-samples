0735a75c1e9ae126db5b447917e791e63b9a89b5
Chenyang Liu <chenyl@microsoft.com>
2018-07-06 10:35:40 +0800

    Format code

--- samples/Serverless/ClientHandler.cs
+++ samples/Serverless/ClientHandler.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using System.Security.Claims;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR.Client;
-using Microsoft.Azure.SignalR.Sample.ConsoleSample;
-using Microsoft.Extensions.CommandLineUtils;
 
 namespace Microsoft.Azure.SignalR.Samples.Serverless
 {
