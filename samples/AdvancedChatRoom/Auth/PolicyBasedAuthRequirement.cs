f9b97f828c03a27dd2448ae8333d93ae817697d1
Kevin Zhao <kevinzha@microsoft.com>
2018-07-06 13:20:14 +0800

    upgrade to latest SDK and minor cleanup

--- samples/AdvancedChatRoom/Auth/PolicyBasedAuthRequirement.cs
+++ samples/AdvancedChatRoom/Auth/PolicyBasedAuthRequirement.cs
@@ -1,18 +1,11 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 
 namespace Microsoft.Azure.SignalR.Samples.AdvancedChatRoom
 {
     public class PolicyBasedAuthRequirement : IAuthorizationRequirement
     {
-        public PolicyBasedAuthRequirement()
-        {
-        }
     }
 }
--- samples/AdvancedChatRoom/build-image.sh
+++ samples/AdvancedChatRoom/build-image.sh
@@ -5,7 +5,7 @@ set -x
 DIR=$(cd `dirname $0`; pwd)
 pushd ${DIR}
 
-declare -a SDKVersion=("1.0.0-preview1-10009" "1.0.0-preview1-10011")
+declare -a SDKVersion=("1.0.0-preview1-10009" "1.0.0-preview1-10011" "1.0.0-preview1-10015")
 
 for version in "${SDKVersion[@]}"; do
     docker build -t signalr-advancedchatroom:$version --build-arg SDKVersion=$version .
