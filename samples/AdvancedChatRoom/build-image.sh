38c08a4f2bf433b0e7a7c78f4c915239d994550a
Kevin Zhao <kvn.zhl@gmail.com>
2018-09-14 14:28:20 +0800

    Update SignalR Service SDK to 1.0.0-preview1-10200 (#51)

--- samples/AdvancedChatRoom/build-image.sh
+++ samples/AdvancedChatRoom/build-image.sh
@@ -5,7 +5,7 @@ set -x
 DIR=$(cd `dirname $0`; pwd)
 pushd ${DIR}
 
-declare -a SDKVersion=("1.0.0-preview1-10009" "1.0.0-preview1-10011" "1.0.0-preview1-10015")
+declare -a SDKVersion=("1.0.0-preview1-10009" "1.0.0-preview1-10011" "1.0.0-preview1-10015" "1.0.0-preview1-10200")
 
 for version in "${SDKVersion[@]}"; do
     docker build -t signalr-advancedchatroom:$version --build-arg SDKVersion=$version .
