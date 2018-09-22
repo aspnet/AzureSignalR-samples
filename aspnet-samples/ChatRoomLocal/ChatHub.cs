7b79ef8d00ac846c2234cfc588a172143e48b3df
Liangying.Wei <lianwei@microsoft.com>
2018-09-22 09:47:35 +0800

    Add a very simple sample code for ASP.NET Azure SignalR support (#52)
    * Add aspnet signalr samples
    * switch to nuget version

---
+++ aspnet-samples/ChatRoomLocal/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace ChatRoom
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage(name, message);
+        }
+    }
+}
