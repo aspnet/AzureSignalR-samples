6db98aaca5fe9c9470f51e41e3cd76cb509761a1
Wanpeng Li <wanl@microsoft.com>
2019-03-22 12:22:45 +0800

    remove dup lisence header

--- samples/Management/NegotiationServer/Startup.cs
+++ samples/Management/NegotiationServer/Startup.cs
@@ -1,9 +1,6 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
