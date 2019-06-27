78b19176ca4aa27cbcde78461154cd37b9205f4f
Jialin Xin <jixin@microsoft.com>
2019-06-27 17:24:56 +0800

    add chat tab in blazor sample

--- samples/ServerSideBlazor/Startup.cs
+++ samples/ServerSideBlazor/Startup.cs
@@ -1,14 +1,11 @@
-// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using ServerSideBlazor.Data;
@@ -17,12 +14,19 @@ namespace ServerSideBlazor
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
-            services.AddServerSideBlazor().AddSignalR().AddAzureSignalR();
+            services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
         }
 
@@ -35,19 +39,21 @@ namespace ServerSideBlazor
             }
             else
             {
+                app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
-
             app.UseStaticFiles();
 
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapDefaultControllerRoute();
                 endpoints.MapBlazorHub();
+                endpoints.MapHub<Hubs.ChatHub>("/chathub");
                 endpoints.MapFallbackToPage("/_Host");
             });
         }
