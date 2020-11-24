0408d0ecb49f94a2136f4f658b1bf74c4a6bed54
Chenyang Liu <chenyl@microsoft.com>
2020-11-24 11:23:34 +0800

    Add bi-direction serverless sample together with msi and key vault reference (#136)
    * Add sample
    
    * Add msi
    
    * Add sample of keyvault reference
    
    * Update local setting
    
    * Update gitignore
    
    * Revert changed
    
    * Update

---
+++ samples/BidirectionChat/csharp/Function.cs
@@ -0,0 +1,127 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FunctionApp
+{
+    public class SimpleChat : ServerlessHub
+    {
+        private const string NewMessageTarget = "newMessage";
+        private const string NewConnectionTarget = "newConnection";
+
+        [FunctionName("index")]
+        public IActionResult GetHomePage([HttpTrigger(AuthorizationLevel.Anonymous)]HttpRequest req, ExecutionContext context)
+        {
+            var path = Path.Combine(context.FunctionAppDirectory, "content", "index.html");
+            Console.WriteLine(path);
+            return new ContentResult
+            {
+                Content = File.ReadAllText(path),
+                ContentType = "text/html",
+            };
+        }
+
+        [FunctionName("negotiate")]
+        public SignalRConnectionInfo Negotiate([HttpTrigger(AuthorizationLevel.Anonymous)]HttpRequest req)
+        {
+            return Negotiate(req.Headers["x-ms-signalr-user-id"], GetClaims(req.Headers["Authorization"]));
+        }
+
+        [FunctionName(nameof(OnConnected))]
+        public async Task OnConnected([SignalRTrigger]InvocationContext invocationContext, ILogger logger)
+        {
+            invocationContext.Headers.TryGetValue("Authorization", out var auth);
+            await Clients.All.SendAsync(NewConnectionTarget, new NewConnection(invocationContext.ConnectionId, auth));
+            logger.LogInformation($"{invocationContext.ConnectionId} has connected");
+        }
+
+        [FunctionAuthorize]
+        [FunctionName(nameof(Broadcast))]
+        public async Task Broadcast([SignalRTrigger]InvocationContext invocationContext, string message, ILogger logger)
+        {
+            await Clients.All.SendAsync(NewMessageTarget, new NewMessage(invocationContext, message));
+            logger.LogInformation($"{invocationContext.ConnectionId} broadcast {message}");
+        }
+
+        [FunctionName(nameof(SendToGroup))]
+        public async Task SendToGroup([SignalRTrigger]InvocationContext invocationContext, string groupName, string message)
+        {
+            await Clients.Group(groupName).SendAsync(NewMessageTarget, new NewMessage(invocationContext, message));
+        }
+
+        [FunctionName(nameof(SendToUser))]
+        public async Task SendToUser([SignalRTrigger]InvocationContext invocationContext, string userName, string message)
+        {
+            await Clients.User(userName).SendAsync(NewMessageTarget, new NewMessage(invocationContext, message));
+        }
+
+        [FunctionName(nameof(SendToConnection))]
+        public async Task SendToConnection([SignalRTrigger]InvocationContext invocationContext, string connectionId, string message)
+        {
+            await Clients.Client(connectionId).SendAsync(NewMessageTarget, new NewMessage(invocationContext, message));
+        }
+
+        [FunctionName(nameof(JoinGroup))]
+        public async Task JoinGroup([SignalRTrigger]InvocationContext invocationContext, string connectionId, string groupName)
+        {
+            await Groups.AddToGroupAsync(connectionId, groupName);
+        }
+
+        [FunctionName(nameof(LeaveGroup))]
+        public async Task LeaveGroup([SignalRTrigger]InvocationContext invocationContext, string connectionId, string groupName)
+        {
+            await Groups.RemoveFromGroupAsync(connectionId, groupName);
+        }
+
+        [FunctionName(nameof(JoinUserToGroup))]
+        public async Task JoinUserToGroup([SignalRTrigger]InvocationContext invocationContext, string userName, string groupName)
+        {
+            await UserGroups.AddToGroupAsync(userName, groupName);
+        }
+
+        [FunctionName(nameof(LeaveUserFromGroup))]
+        public async Task LeaveUserFromGroup([SignalRTrigger]InvocationContext invocationContext, string userName, string groupName)
+        {
+            await UserGroups.RemoveFromGroupAsync(userName, groupName);
+        }
+
+        [FunctionName(nameof(OnDisconnected))]
+        public void OnDisconnected([SignalRTrigger]InvocationContext invocationContext)
+        {
+        }
+
+        private class NewConnection
+        {
+            public string ConnectionId { get; }
+
+            public string Authentication { get; }
+
+            public NewConnection(string connectionId, string authentication)
+            {
+                ConnectionId = connectionId;
+                Authentication = authentication;
+            }
+        }
+
+        private class NewMessage
+        {
+            public string ConnectionId { get; }
+            public string Sender { get; }
+            public string Text { get; }
+
+            public NewMessage(InvocationContext invocationContext, string message)
+            {
+                Sender = string.IsNullOrEmpty(invocationContext.UserId) ? string.Empty : invocationContext.UserId;
+                ConnectionId = invocationContext.ConnectionId;
+                Text = message;
+            }
+        }
+    }
+}
