5860d47c563a2f7b6d143ccc0e0e22ed16a3131f
Juntao Peng <pengjuntao2016@foxmail.com>
2020-11-30 10:20:52 +0800

    Remove files introduced by accidental checkout

--- samples/MobileChatRoom/AndroidChatRoomClient/app/src/main/java/com/microsoft/signalr/androidchatroom/fragment/chatrecyclerview/RecyclerViewItemClickListener.java
+++
@@ -1,7 +0,0 @@
-package com.microsoft.signalr.androidchatroom.fragment.chatrecyclerview;
-
-import com.microsoft.signalr.androidchatroom.message.Message;
-
-public interface RecyclerViewItemClickListener {
-    void onClickItem(Message message);
-}
--- samples/MobileChatRoom/AndroidChatRoomClient/app/src/main/java/com/microsoft/signalr/androidchatroom/message/SimpleCallback.java
+++
@@ -1,5 +0,0 @@
-package com.microsoft.signalr.androidchatroom.message;
-
-public interface SimpleCallback<T> {
-    void run(T t);
-}
