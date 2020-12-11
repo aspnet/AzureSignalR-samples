8d03727f2fe6a1873ae6f688955c776abbf05d9b
Juntao Peng <pengjuntao2016@foxmail.com>
2020-12-11 11:48:57 +0800

    Remove files

--- samples/MobileChatRoom/AndroidChatRoomClient/app/src/main/java/com/microsoft/signalr/androidchatroom/view/chatrecyclerview/RecyclerViewItemClickListener.java
+++
@@ -1,7 +0,0 @@
-package com.microsoft.signalr.androidchatroom.view.chatrecyclerview;
-
-import com.microsoft.signalr.androidchatroom.model.entity.Message;
-
-public interface RecyclerViewItemClickListener {
-    void onClickItem(Message message);
-}
