92e249ca1a65ddecaf60a157f5979361ffade6d3
Chenyang Liu <chenyl@microsoft.com>
2021-06-15 16:30:41 +0800

    Add new quick start samples

---
+++ samples/QuickStartServerless/javascript/broadcast/index.js
@@ -0,0 +1,30 @@
+var https = require('https');
+
+module.exports = function (context) {
+    var req = https.request("https://api.github.com/repos/azure/azure-webpubsub", {
+        method: 'GET',
+        headers: {'User-Agent': 'serverless'}
+    }, res => {
+        var body = "";
+
+        res.on('data', data => {
+            body += data;
+        });
+        res.on("end", () => {
+            var jbody = JSON.parse(body);
+            context.bindings.signalRMessages = [{
+                "target": "newMessage",
+                "arguments": [ `Current star count of https://github.com/Azure/azure-webpubsub is: ${jbody['stargazers_count']}` ]
+            }]
+            context.done();
+        });
+    }).on("error", (error) => {
+        context.log(error);
+        context.res = {
+          status: 500,
+          body: error
+        };
+        context.done();
+    });
+    req.end();
+}
