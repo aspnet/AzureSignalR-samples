1406efca779b20e32c8d509bfa48f02bebf95144
Chenyang Liu <chenyl@microsoft.com>
2021-06-15 16:30:41 +0800

    Add new quick start samples

---
+++ samples/QuickStartServerless/csharp/Function.cs
@@ -0,0 +1,60 @@
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
+using Newtonsoft.Json;
+
+namespace CSharp
+{
+    public static class Function
+    {
+        private static HttpClient httpClient = new HttpClient();
+
+        [FunctionName("index")]
+        public static IActionResult GetHomePage([HttpTrigger(AuthorizationLevel.Anonymous)]HttpRequest req, ExecutionContext context)
+        {
+            var path = Path.Combine(context.FunctionAppDirectory, "content", "index.html");
+            return new ContentResult
+            {
+                Content = File.ReadAllText(path),
+                ContentType = "text/html",
+            };
+        }
+
+        [FunctionName("negotiate")]
+        public static SignalRConnectionInfo Negotiate( 
+            [HttpTrigger(AuthorizationLevel.Anonymous)] HttpRequest req,
+            [SignalRConnectionInfo(HubName = "serverlessSample")] SignalRConnectionInfo connectionInfo)
+        {
+            return connectionInfo;
+        }
+
+        [FunctionName("getStars")]
+        public static async Task Broadcast([TimerTrigger("*/5 * * * * *")] TimerInfo myTimer,
+        [SignalR(HubName = "serverlessSample")] IAsyncCollector<SignalRMessage> signalRMessages)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://api.github.com/repos/azure/azure-webpubsub");
+            request.Headers.UserAgent.ParseAdd("Serverless");
+            var response = await httpClient.SendAsync(request);
+            var result = JsonConvert.DeserializeObject<GitResult>(await response.Content.ReadAsStringAsync());
+            await signalRMessages.AddAsync(
+                new SignalRMessage
+                {
+                    Target = "newMessage",
+                    Arguments = new[] { $"Current star count of https://github.com/Azure/azure-webpubsub is: {result.StartCount}" }
+                });
+        }
+
+        private class GitResult
+        {
+            [JsonRequired]
+            [JsonProperty("stargazers_count")]
+            public string StartCount { get; set; }
+        }
+    }
+}
---
+++ samples/QuickStartServerless/java/src/main/java/com/signalr/Function.java
@@ -0,0 +1,72 @@
+package com.signalr;
+
+import com.google.gson.Gson;
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.HttpMethod;
+import com.microsoft.azure.functions.HttpRequestMessage;
+import com.microsoft.azure.functions.HttpResponseMessage;
+import com.microsoft.azure.functions.HttpStatus;
+import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.HttpTrigger;
+import com.microsoft.azure.functions.annotation.TimerTrigger;
+import com.microsoft.azure.functions.signalr.*;
+import com.microsoft.azure.functions.signalr.annotation.*;
+
+import org.apache.commons.io.IOUtils;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.nio.charset.StandardCharsets;
+import java.util.Optional;
+
+public class Function {
+    @FunctionName("index")
+    public HttpResponseMessage run(
+            @HttpTrigger(
+                name = "req",
+                methods = {HttpMethod.GET},
+                authLevel = AuthorizationLevel.ANONYMOUS)HttpRequestMessage<Optional<String>> request,
+            final ExecutionContext context) throws IOException {
+        
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("content/index.html");
+        String text = IOUtils.toString(inputStream, StandardCharsets.UTF_8.name());
+        return request.createResponseBuilder(HttpStatus.OK).header("Content-Type", "text/html").body(text).build();
+    }
+
+    @FunctionName("negotiate")
+    public SignalRConnectionInfo negotiate(
+            @HttpTrigger(
+                name = "req",
+                methods = { HttpMethod.POST },
+                authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> req,
+            @SignalRConnectionInfoInput(
+                name = "connectionInfo",
+                hubName = "serverless") SignalRConnectionInfo connectionInfo) {
+                
+        return connectionInfo;
+    }
+
+    @FunctionName("broadcast")
+    @SignalROutput(name = "$return", hubName = "serverless")
+    public SignalRMessage broadcast(
+        @TimerTrigger(name = "timeTrigger", schedule = "*/5 * * * * *") String timerInfo) throws IOException, InterruptedException {
+        
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest req = HttpRequest.newBuilder().uri(URI.create("https://api.github.com/repos/azure/azure-webpubsub")).header("User-Agent", "serverless").build();
+        HttpResponse<String> res = client.send(req, BodyHandlers.ofString());
+        Gson gson = new Gson();
+        GitResult result = gson.fromJson(res.body(), GitResult.class);
+        return new SignalRMessage("newMessage", "Current start count of https://github.com/Azure/azure-webpubsub is:".concat(result.stargazers_count));
+    }
+
+    class GitResult {
+        public String stargazers_count;
+    }
+}
