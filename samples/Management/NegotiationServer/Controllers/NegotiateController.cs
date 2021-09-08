7791af2af37f47c2e4016aef5347f92aa4e82ca1
yzt <zityang@microsoft.com>
2021-09-08 17:01:35 +0800

    Add new management sample (#163)
    * Add and update Management sample for 1.10.0
    
    * Update and add md files
    
    * add file header
    
    * fix indention
    
    * fix
    
    * Remove Legacy Sample
    
    * fix
    
    * add another hub
    
    * Fix
    
    * Fix readme.md
    
    * Add more comments
    
    * fix doc
    
    * Add gitignore

---
+++ samples/Management/NegotiationServer/Controllers/NegotiateController.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.SignalR.Management;
+
+namespace NegotiationServer.Controllers
+{
+    [ApiController]
+    public class NegotiateController : ControllerBase
+    {
+        private readonly ServiceHubContext _messageHubContext;
+        private readonly ServiceHubContext _chatHubContext;
+
+        public NegotiateController(IHubContextStore store)
+        {
+            _messageHubContext = store.MessageHubContext;
+            _chatHubContext = store.ChatHubContext;
+        }
+
+        [HttpPost("message/negotiate")]
+        public Task<ActionResult> MessageHubNegotiate(string user)
+        {
+            return NegotiateBase(user, _messageHubContext);
+        }
+
+        //This API is not used. Just demonstrate a way to have multiple hubs.
+        [HttpPost("chat/negotiate")]
+        public Task<ActionResult> ChatHubNegotiate(string user)
+        {
+            return NegotiateBase(user, _chatHubContext);
+        }
+
+        private async Task<ActionResult> NegotiateBase(string user, ServiceHubContext serviceHubContext)
+        {
+            if (string.IsNullOrEmpty(user))
+            {
+                return BadRequest("User ID is null or empty.");
+            }
+
+            var negotiateResponse = await serviceHubContext.NegotiateAsync(new() { UserId = user });
+
+            return new JsonResult(new Dictionary<string, string>()
+            {
+                { "url", negotiateResponse.Url },
+                { "accessToken", negotiateResponse.AccessToken }
+            });
+        }
+    }
+}
--- samples/Management/SignalRClient/Program.cs
+++ samples/Management/SignalRClient/Program.cs
@@ -13,26 +13,26 @@ namespace SignalRClient
 {
     class Program
     {
-        private const string DefaultHubEndpoint = "http://localhost:5000/ManagementSampleHub";
+        private const string MessageHubEndpoint = "http://localhost:5000/Message";
         private const string Target = "Target";
         private const string DefaultUser = "User";
 
         static void Main(string[] args)
         {
-            var app = new CommandLineApplication();
-            app.FullName = "Azure SignalR Management Sample: SignalR Client Tool";
+            var app = new CommandLineApplication
+            {
+                FullName = "Azure SignalR Management Sample: SignalR Client Tool"
+            };
             app.HelpOption("--help");
 
-            var hubEndpointOption = app.Option("-h|--hubEndpoint", $"Set hub endpoint. Default value: {DefaultHubEndpoint}", CommandOptionType.SingleValue, true);
             var userIdOption = app.Option("-u|--userIdList", "Set user ID list", CommandOptionType.MultipleValue, true);
 
             app.OnExecute(async () =>
             {
-                var hubEndpoint = hubEndpointOption.Value() ?? DefaultHubEndpoint;
-                var userIds = userIdOption.Values != null && userIdOption.Values.Count > 0 ? userIdOption.Values : new List<string>() { "User" };
+                var userIds = userIdOption.Values != null && userIdOption.Values.Count > 0 ? userIdOption.Values : new List<string>() { DefaultUser };
 
                 var connections = (from userId in userIds
-                                   select CreateHubConnection(hubEndpoint, userId)).ToList();
+                                   select CreateHubConnection(MessageHubEndpoint, userId)).ToList();
 
                 await Task.WhenAll(from conn in connections
                                    select conn.StartAsync());
