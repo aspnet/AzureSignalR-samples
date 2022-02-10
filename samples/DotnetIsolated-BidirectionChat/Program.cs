6f8f8fd53ac84614f637a0c9d2c621707d475c84
Zitong Yang <zityang@microsoft.com>
2022-02-10 16:47:27 +0800

    Remove unused pojo in the dotnet isolated bidirectional chat sample

--- samples/DotnetIsolated-BidirectionChat/Program.cs
+++ samples/DotnetIsolated-BidirectionChat/Program.cs
@@ -1,7 +1,4 @@
-using System.Threading.Tasks;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Azure.Functions.Worker.Configuration;
 
 namespace IsolatedModel_BidirectionChat
 {
