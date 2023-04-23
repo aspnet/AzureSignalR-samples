8864ac9a2aa856f7bdf12790278434450668800a
yzt <zityang@microsoft.com>
2023-04-23 16:02:19 +0800

    Add .NET isolated worker function integrated with SignalR management SDK (#224)

---
+++ samples/DotnetIsolated-ClassBased/NewConnection.cs
@@ -0,0 +1,14 @@
+﻿namespace DotnetIsolated_ClassBased;
+
+public class NewConnection
+{
+    public string ConnectionId { get; }
+
+    public string Authentication { get; }
+
+    public NewConnection(string connectionId, string auth)
+    {
+        ConnectionId = connectionId;
+        Authentication = auth;
+    }
+}
---
+++ samples/DotnetIsolated-ClassBased/NewMessage.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Azure.Functions.Worker;
+
+namespace DotnetIsolated_ClassBased;
+public class NewMessage
+{
+    public string ConnectionId { get; }
+    public string Sender { get; }
+    public string Text { get; }
+
+    public NewMessage(SignalRInvocationContext invocationContext, string message)
+    {
+        Sender = string.IsNullOrEmpty(invocationContext.UserId) ? string.Empty : invocationContext.UserId;
+        ConnectionId = invocationContext.ConnectionId;
+        Text = message;
+    }
+}
+
