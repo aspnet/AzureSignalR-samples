52dd37d86c0a51779b50ce13afdf7ab425464675
Liangying.Wei <lianwei@microsoft.com>
2023-11-13 15:17:03 +0800

    Improve the chat sample code and enable HTTPS support

--- samples/ChatRoom/Program.cs
+++ samples/ChatRoom/Program.cs
@@ -2,8 +2,11 @@
 builder.Services.AddSignalR().AddAzureSignalR();
 var app = builder.Build();
 
+app.UseHttpsRedirection();
 app.UseDefaultFiles();
 app.UseStaticFiles();
 app.UseRouting();
+
 app.MapHub<ChatSampleHub>("/chat");
+
 app.Run();
