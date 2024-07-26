77802633135169a2616b1cfc90d9509c1d3f7292
Terence Fan <stdrickforce@gmail.com>
2024-07-26 17:39:05 +0800

    sample (#249)

--- samples/ChatRoom/Program.cs
+++ samples/ChatRoom/Program.cs
@@ -1,5 +1,27 @@
-﻿var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddSignalR().AddAzureSignalR();
+﻿using Azure.Identity;
+
+using Microsoft.Azure.SignalR;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSignalR().AddAzureSignalR(option =>
+{
+    // Here are sample codes for sovereign clouds
+    //
+    // var credentialOptions = new ClientSecretCredentialOptions()
+    // {
+    //     AuthorityHost = AzureAuthorityHosts.AzurePublicCloud,
+    //     // AuthorityHost = AzureAuthorityHosts.AzureGovernment, // for fairfax region
+    //     // AuthorityHost = AzureAuthorityHosts.AzureChina, // for mooncake region
+    // };
+
+    // var tenantId = "";
+    // var clientId = "";
+    // var clientSecret = "";
+
+    // option.Endpoints = new ServiceEndpoint[] {
+    //     new(new Uri("https://<hostname>"), new ClientSecretCredential(tenantId, clientId, clientSecret, credentialOptions))
+    // };
+});
 var app = builder.Build();
 
 app.UseHttpsRedirection();
