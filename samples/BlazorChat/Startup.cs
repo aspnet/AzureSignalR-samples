42b4e405fde6833d6aed267d12bba7075402f5cd
JialinXin <jixin@microsoft.com>
2024-08-05 10:40:52 +0800

    Remove DI. (#290)

--- samples/BlazorChat/Startup.cs
+++ samples/BlazorChat/Startup.cs
@@ -28,7 +28,6 @@ namespace BlazorChat
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSignalR().AddAzureSignalR("***REMOVED***");
             services.AddSingleton<WeatherForecastService>();
         }
 
