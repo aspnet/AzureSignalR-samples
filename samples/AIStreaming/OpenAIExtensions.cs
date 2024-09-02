e71631ca8f784705ec903ed9dce91f256b8bfb93
Chenyang Liu <chenyl@microsoft.com>
2024-09-02 11:25:19 +0800

    Add AI streaming samples

---
+++ samples/AIStreaming/OpenAIExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Azure.AI.OpenAI;
+using Microsoft.Extensions.Options;
+using OpenAI;
+using System.ClientModel;
+
+namespace AIStreaming
+{
+    public static class OpenAIExtensions
+    {
+        public static IServiceCollection AddAzureOpenAI(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services
+                .Configure<OpenAIOptions>(configuration.GetSection("OpenAI"))
+                .AddSingleton<OpenAIClient>(provider =>
+                {
+                    var options = provider.GetRequiredService<IOptions<OpenAIOptions>>().Value;
+                    return new AzureOpenAIClient(new Uri(options.Endpoint), new ApiKeyCredential(options.Key));
+                });
+        }
+
+        public static IServiceCollection AddOpenAI(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services
+                .Configure<OpenAIOptions>(configuration.GetSection("OpenAI"))
+                .AddSingleton<OpenAIClient>(provider =>
+                {
+                    var options = provider.GetRequiredService<IOptions<OpenAIOptions>>().Value;
+                    return new OpenAIClient(new ApiKeyCredential(options.Key));
+                });
+        }
+    }
+}
---
+++ samples/AIStreaming/OpenAIOptions.cs
@@ -0,0 +1,20 @@
+﻿namespace AIStreaming
+{
+    public class OpenAIOptions
+    {
+        /// <summary>
+        /// The endpoint of Azure OpenAI service. Only available for Azure OpenAI.
+        /// </summary>
+        public string? Endpoint { get; set; }
+
+        /// <summary>
+        /// The key of OpenAI service.
+        /// </summary>
+        public string? Key { get; set; }
+
+        /// <summary>
+        /// The model to use.
+        /// </summary>
+        public string? Model { get; set; }
+    }
+}
