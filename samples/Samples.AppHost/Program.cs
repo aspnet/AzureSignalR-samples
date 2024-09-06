5b1d1e75f990dbe23780376bbab809bd7d9d0e11
Liangying Wei <lianwei@microsoft.com>
2024-09-06 15:42:49 +0800

    Update launch settings for the samples

--- samples/Samples.AppHost/Program.cs
+++ samples/Samples.AppHost/Program.cs
@@ -5,16 +5,16 @@ var builder = DistributedApplication.CreateBuilder(args);
 var asrs = builder.AddAzureSignalR(ServiceConstants.SignalRServiceName, (_, _, r) => r.AssignProperty(s => s.Sku.Name, "'Standard_S1'"));
 #pragma warning restore AZPROVISION001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
 
-builder.AddProject<ChatRoom>(nameof(ChatRoom)).WithReference(asrs);
+// Use WithReplicas to start multiple instances of ChatRoom
+builder.AddProject<ChatRoom>(nameof(ChatRoom)).WithReplicas(2).WithReference(asrs);
 builder.AddProject<AckableChatRoom>(nameof(AckableChatRoom)).WithReference(asrs);
-//builder.AddProject<AdvancedChatRoom>(nameof(AdvancedChatRoom)).WithReference(asrs);
-//builder.AddProject<BlazorChat>(nameof(BlazorChat)).WithReference(asrs);
-//builder.AddProject<FlightMap>(nameof(FlightMap)).WithReference(asrs);
+builder.AddProject<AdvancedChatRoom>(nameof(AdvancedChatRoom)).WithReference(asrs);
+builder.AddProject<BlazorChat>(nameof(BlazorChat)).WithReference(asrs);
+builder.AddProject<FlightMap>(nameof(FlightMap)).WithReference(asrs);
 
-//builder.AddProject<ClientInvocationSample>(nameof(ClientInvocationSample)).WithReference(asrs);
+builder.AddProject<ClientInvocationSample>(nameof(ClientInvocationSample)).WithReference(asrs);
 
-//builder.AddProject<GitHubChat>(nameof(GitHubChat)).WithReference(asrs);
-//builder.AddProject<ServerSideBlazor>(nameof(ServerSideBlazor)).WithReference(asrs);
-//builder.AddProject<SimpleEcho>(nameof(SimpleEcho)).WithReference(asrs);
+builder.AddProject<GitHubChat>(nameof(GitHubChat)).WithReference(asrs);
+builder.AddProject<ServerSideBlazor>(nameof(ServerSideBlazor)).WithReference(asrs);
 
 builder.Build().Run();
