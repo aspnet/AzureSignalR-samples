7b6d473490b468449d3bc17eb42f83d2d5cf284e
Ken Chen <kenchen@microsoft.com>
2025-04-25 12:24:54 +0800

    Change to top level statements and remove Startup.cs

--- samples/Whiteboard/Program.cs
+++ samples/Whiteboard/Program.cs
@@ -1,19 +1,19 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Azure.SignalR.Samples.Whiteboard;
 
-namespace Microsoft.Azure.SignalR.Samples.Whiteboard;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSingleton<Diagram>();
+builder.Services.AddMvc();
+builder.Services.AddSignalR().AddAzureSignalR();
 
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        CreateWebHostBuilder(args).Build().Run();
-    }
+var app = builder.Build();
+app.UseRouting();
+app.UseFileServer();
+app.MapControllers();
+app.MapHub<DrawHub>("/draw");
 
-    public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-        WebHost.CreateDefaultBuilder(args)
-            .UseStartup<Startup>();
-}
+app.Run();
