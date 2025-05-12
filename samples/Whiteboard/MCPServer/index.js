e88246cad6cc8dbb76d9693561b28126d29e9571
Ken Chen <kenchen@microsoft.com>
2025-05-12 14:51:58 +0800

    Add fill functionality to whiteboard (#313)
    1. Allow selecting fill color for shapes
    2. Improve the Azure deploy steps in README

--- samples/Whiteboard/MCPServer/index.js
+++ samples/Whiteboard/MCPServer/index.js
@@ -10,15 +10,18 @@ const logger = new class {
   log = (level, message) => level > 1 && console.error(`[${level}] ${message}`);
 };
 
-const connection = new HubConnectionBuilder().withUrl(`${process.env['WHITEBOARD_ENDPOINT'] || 'http://localhost:5000'}/draw`).withAutomaticReconnect().configureLogging(logger).build();
+const endpoint = process.env['WHITEBOARD_ENDPOINT'] || 'http://localhost:5000';
+
+const connection = new HubConnectionBuilder().withUrl(`${endpoint}/draw`).withAutomaticReconnect().configureLogging(logger).build();
 
 const server = new McpServer({
   name: 'Whiteboard',
   version: '1.0.0'
 });
 
-let color = z.string().describe('color of the shape, valid values are: black, grey, darkred, red, orange, yellow, green, deepskyblue, indigo, purple');
-let width = z.number().describe('width of the shape, valid values are: 1, 2, 4, 8');
+let color = z.string().describe('line color of the shape, value should be a valid CSS color');
+let fill = z.string().describe('fill color of the shape, value should be a valid CSS color');
+let width = z.number().describe('width of the shape in pixels');
 let point = z.object({
   x: z.number().describe('x coordinate of the point, 0 denotes the left edge of the whiteboard'),
   y: z.number().describe('y coordinate of the point, 0 denotes the top edge of the whiteboard')
@@ -34,7 +37,7 @@ server.tool(
   'add_or_update_polyline', 'add or update a polyline on whiteboard',
   {
     id, polyline: z.object({
-      color, width,
+      color, width, fill,
       points: z.array(point).describe('array of points that define the polyline')
     })
   },
@@ -47,7 +50,7 @@ server.tool(
   'add_or_update_line', 'add or update a line on whiteboard',
   {
     id, line: z.object({
-      color, width,
+      color, width, fill,
       start: point.describe('start point of the line'),
       end: point.describe('end point of the line')
     })
@@ -61,7 +64,7 @@ server.tool(
   'add_or_update_circle', 'add or update a circle on whiteboard',
   {
     id, circle: z.object({
-      color, width,
+      color, width, fill,
       center: point.describe('center point of the circle'),
       radius: z.number().describe('radius of the circle')
     })
@@ -75,9 +78,9 @@ server.tool(
   'add_or_update_rect', 'add or update a rectangle on whiteboard',
   {
     id, rect: z.object({
-      color, width,
+      color, width, fill,
       topLeft: point.describe('top left corner of the rectangle'),
-      bottomRight: point.describe('bottom right of the rectangle')
+      bottomRight: point.describe('bottom right corner of the rectangle')
     })
   },
   async ({ id, rect }) => {
@@ -89,9 +92,9 @@ server.tool(
   'add_or_update_ellipse', 'add or update an ellipse on whiteboard',
   {
     id, ellipse: z.object({
-      color, width,
+      color, width, fill,
       topLeft: point.describe('top left corner of the bounding rectangle of the ellipse'),
-      bottomRight: point.describe('bottom right of the bounding rectangle of the ellipse')
+      bottomRight: point.describe('bottom right corner of the bounding rectangle of the ellipse')
     })
   },
   async ({ id, ellipse }) => {
@@ -120,7 +123,7 @@ const transport = new StdioServerTransport();
 await server.connect(transport);
 
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
-for (;;) {
+for (; ;) {
   try {
     await connection.start();
     break;
